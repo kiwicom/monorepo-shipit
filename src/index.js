b33a397afbee19b6a10edc4fe141e9b2f50ea7af
Martin Zlámal <martin.zlamal@kiwi.com>
2019-04-06 10:57:19 -0600

    Monorepo Shipit: configure Git user for pushes to GitHub
    kiwicom-source-id: 786216f45cda1a6dc4738f313b4f7b83df730afb

--- src/index.js
+++ src/index.js
@@ -11,15 +11,13 @@ import { invariant } from '@kiwicom/js';
 import Sync from './Sync';
 import OSSPackages from '../../../open-source';
 
+// TODO: fail on errors (see: https://gitlab.skypicker.com/incubator/universe/-/jobs/4646614)
 // TODO: fix fucked-up packages (where we lost Git history because of multiple roots):
 // - src/packages/bc-checker -> src/packages/graphql-bc-checker
 // - src/packages/global-id -> src/packages/graphql-global-id
 // - src/_graphql-resolve-wrapper -> src/packages/graphql-resolve-wrapper
 // - src/packages/npm-publisher -> src/packages/monorepo-npm-publisher
 // - vault2env (prolly not fixable, I forgot to import this package with history from the previous repository - no biggie)
-
-// yarn monorepo-babel-node src/packages/monorepo-shipit/src/index.js
-// TODO: changeset.withDebugMessage
 
 // TODO: we could (should) eventually keep the temp directory, cache it and just update it
 const tmpdir = fs.mkdtempSync(path.join(os.tmpdir(), 'kiwicom-shipit-'));
@@ -36,6 +34,23 @@ for (const [, config] of OSSPackages.entries()) {
   const localDirectory = match?.groups?.localDirectory;
   invariant(localDirectory != null, 'Cannot resolve local directory.');
   const clonedir = path.join(tmpdir, localDirectory);
+
+  ChildProcess.executeSystemCommand(
+    'git',
+    ['config', 'user.email', 'mrtnzlml+kiwicom-github-bot@gmail.com'],
+    {
+      stdio: 'inherit',
+      cwd: clonedir,
+    },
+  );
+  ChildProcess.executeSystemCommand(
+    'git',
+    ['config', 'user.name', 'kiwicom-github-bot'],
+    {
+      stdio: 'inherit',
+      cwd: clonedir,
+    },
+  );
 
   new Sync({
     clonedir,
