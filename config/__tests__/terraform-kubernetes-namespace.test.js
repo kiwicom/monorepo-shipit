55dae7b29951e9c4b5c265d9f6958f7a4c6eb89a
Adam Janis <adam.janis@kiwi.com>
2019-06-19 17:24:25 +0200

    terraform-kubernetes-namespace: add tests and codeowners
    kiwicom-source-id: 7928676e262962f4f6d391b0fbe7badbe868947b

---
+++ config/__tests__/terraform-kubernetes-namespace.test.js
@@ -0,0 +1,24 @@
+// @flow strict-local
+
+import path from 'path';
+
+import testExportedPaths from './testExportedPaths';
+
+testExportedPaths(
+  path.join(__dirname, '..', 'terraform-kubernetes-namespace.js'),
+  [
+    [
+      'src/platform/terraform-kubernetes-namespace/outputs.tf',
+      'outputs.tf',
+    ],
+    ['src/platform/terraform-kubernetes-namespace/main.tf', 'main.tf'],
+    [
+      'src/platform/terraform-kubernetes-namespace/variables.tf',
+      'variables.tf',
+    ],
+
+    // invalid cases:
+    ['src/packages/xyz/outsideScope.js', undefined], // correctly deleted
+    ['package.json', undefined], // correctly deleted
+  ],
+);
